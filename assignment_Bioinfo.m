cc09852837ebd21ff1d73d3d1ef94f4575264fe8
noedout <doutrelepontnoe@gmail.com>
2024-05-08 11:37:14 +0200

    Test 1
    Copy paste my tests from external source

--- assignment_Bioinfo.m
+++ assignment_Bioinfo.m
@@ -12,13 +12,23 @@ y0  = [10 7 5]; % initial population size [population size]
 tspan = [t0 tfinal];
 
 % Assigning values to the parameters: 
-a1 = 5;
-a2  = 0.1;
-b1 = 3;
-b2 = 0.1;
-d1 = 0.4;
-d2 = 0.01;
-
+y0  = [3  4  1];
+a1 = [4];
+a2  = [0.3];
+b1 = [6];
+b2 = [5];
+d1 = [0.4];
+d2 = [0.5];
+% The parameters are the following: 
+% a1 : Growth rate of hares due to the availability of plants.
+% b1 : Represents the effect of predation on hares by lynx.
+% d1 : Death rate of hares due to natural causes or predation.
+% a2 : Growth rate of lynx due to the availability of hares.
+% b2 : Represents the effect of competition among lynx for hares.
+% d2 : Death rate of lynx due to natural causes or lack of prey.
+%
+% We will call the plant mass y(1), the number of herbivores y(2) , and the
+% number of carnivores y(3) 
 % Simulating the population's evolution: 
 
 [t,y] = ode23(@(t,y) plants_hare_lynx(t,y,a1,a2,b1,b2,d1,d2),tspan,y0);
@@ -38,5 +48,4 @@ dydt(2) = ((a1*y(1))/(1+b1*y(1)))*y(2)-d1*y(2)-(a2*y(2)/(1+b2*y(2)))*y(3);
 dydt(3) = (a2*y(2)/(1+b2*y(2)))*y(3) - d2*y(3);
 
 end
-
 
